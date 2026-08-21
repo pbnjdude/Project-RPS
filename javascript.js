acc571cf16a15a1bd2b447ad963669141a0a76ba
pbnjdude <pbnjgod@gmail.com>
2026-08-21 15:02:56 +0200

    show results of computer selection, game and, result
    add alerts to show what computer chose
    add alerts to show what the result of a game
    add alert to so final result

--- javascript.js
+++ javascript.js
@@ -25,30 +25,30 @@ function playGame(){
      function playRound(humanChoice, computerChoice){
         if (humanChoice === computerChoice){
             tie += 1; 
-            return "It's a tie, you are both losers!"
+            alert("It's a tie, you are both losers!")
         }else if (humanChoice === "rock"){
             if (computerChoice === "paper"){
                 computerScore += 1; 
-                return "You lose, Paper beats Rock!"
+                alert("You lose, Paper beats Rock!")
             }else{
                 humanScore += 1; 
-                return "You win! Rock beats Scissors."
+                alert("You win! Rock beats Scissors.")
             }
         }else if (humanChoice === "scissors"){
             if (computerChoice === "rock"){
                 computerScore += 1; 
-                return "You lose, Rock beats Scissors!"
+                alert("You lose, Rock beats Scissors!")
             }else{
                 humanScore += 1; 
-                return "You win, Scissors beats Paper!"
+                alert("You win, Scissors beats Paper!")
             }
         }else if (humanChoice === "paper"){
             if (computerChoice === "scissors"){
                 computerScore += 1; 
-                return "You lose, Scissors beats Paper!"
+                alert("You lose, Scissors beats Paper!")
             }else{
                 humanScore += 1; 
-                return "You win, Paper beats Rock!"
+                alert("You win, Paper beats Rock!")
             }
         }else{
             return "Please type Paper, Rock, or Scissors and not whatever nonsense you have writen. Thanks"
@@ -60,10 +60,10 @@ function playGame(){
     let tie = 0; 
     
     for (i = 0; i < 5; i++) {
-        let compChoice = getComputerChoice(); 
-        let humChoice = getHumanChoice(); 
-
-        let result = playRound(humChoice ,compChoice); 
+        const humanSelection = getHumanChoice(); 
+        const computerSelection = getComputerChoice(); 
+        alert("Computer selected: " + computerSelection); 
+        let result = playRound(humanSelection ,computerSelection); 
         
     }
     let resultText = "You scored :" + humanScore + " The computer scored :"+ computerScore + " You tied " + tie + " games.";
@@ -79,5 +79,5 @@ function playGame(){
 
 
 
-let check = playGame(); 
-console.log(check); 
+alert(playGame()); 
+
